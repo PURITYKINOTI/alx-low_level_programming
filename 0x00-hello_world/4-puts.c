57934cc43d4ab0b76947035d56bc2c2f918ba3b1
PURITYKINOTI <karwithapurity9@gmail.com>
2022-09-08 05:09:29 -0700

    4puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * description: this is alx low level programming
+ * Return: if the program is successful
+ */
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
