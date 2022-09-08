df782ced6440255d997af9e897c01a788b248e05
PURITYKINOTI <karwithapurity9@gmail.com>
2022-09-08 05:19:27 -0700

    6 size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints "with good grammar, but the outcome is
+ * a perfect art," followed by a new line.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("with good grammar, but the outcome is a perfect art,\n");
+	return (0);
+}
