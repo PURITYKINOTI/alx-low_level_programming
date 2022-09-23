584bed2cb4abed673e3a466ebe5e606c92775ff5
PURITYKINOTI <karwithapurity9@gmail.com>
2022-09-23 11:12:11 -0700

    103infinite

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -2,42 +2,51 @@
 #include <stdio.h>
 
 /**
- * print_buffer - prints a buffer
- * @b: buffer.
- * @size: size of buffer.
- * Return: no return.
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: the buffer that the function will use to store the result
+ * @size_r: buffer size
+ *
+ * Return: the pointer to dest
  */
-void print_buffer(char *b, int size)
-{
-	int j, k, l;
 
-	if (size <= 0)
-		printf("\n");
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int c = 0, b = 0, p, g, d, h, add = 0;
+
+	while (*(n1 + c) != '\0')
+		c++;
+	while (*(n2 + b) != '\0')
+		b++;
+	if (c >= b)
+		g = c;
 	else
+		g = b;
+	if (size_r <= g + 1)
+		return (0);
+	r[g + 1] = '\0';
+	c--, b--, size_r--;
+	d = *(n1 + c) - 48, h = *(n2 + b) - 48;
+	while (bg >= 0)
 	{
-		for (j = 0; j < size; j += 10)
-		{
-			printf("%.8x:", j);
-			for (k = j; k < j + 10; k++)
-			{
-				if (k % 2 == 0)
-					printf(" ");
-				if (k < size)
-					printf("%.2x", *(b + k));
-				else
-					printf("  ");
-			}
-			printf(" ");
-			for (l = j; l < j + 10; l++)
-			{
-				if (l >= size)
-					break;
-				if (*(b + l) < 32 || *(b + l) > 126)
-					printf("%c", '.');
-				else
-					printf("%c", *(b + l));
-			}
-			printf("\n");
-		}
+		p = d + h + add;
+		if (p >= 10)
+			add = p / 10;
+		else
+			add = 0;
+		if (op > 0)
+			*(r + bg) = '0';
+		if (c > 0)
+			c--, d = *(n1 + c) - 48;
+		else
+			d = 0;
+		if (b > 0)
+			b--, h = *(n2 + b) - 48;
+		g--, size_r--;
 	}
+	if (*(r) == '0')
+		return (r + 1);
+	else
+		return (r);
 }
