d041bc2109ed66fdd39633be0989dbd59db27dea
PURITYKINOTI <karwithapurity9@gmail.com>
2022-09-28 08:36:47 -0700

    3fact

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
 
 /**
- * factorial - returns the factorial of a given number.
- * @n: input number.
- * Return: factorial of the number.
+ * factorial - returns the factorial of a number
+ * @n: number to return the factorial from
+ *
+ * Return: factorial of n
  */
 int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
-	else if (n == 0)
+	if (n == 0)
 		return (1);
-	else
-		return (n * factorial(n - 1));
+	return (n * factorial(n - 1));
 }
